bcaa523cd12a52e80c840788311fbba0363d087d
alecyrus2001 <aldecyrus2001@gmail.com>
2025-06-16 19:31:00 +0800

    6/16/2025

--- Webpage/resources/js/pages/contacts.tsx
+++ Webpage/resources/js/pages/contacts.tsx
@@ -1,7 +1,11 @@
+import Modal from '@/components/custom/universal-modal'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Select, SelectItem, SelectTrigger, SelectValue, SelectContent } from '@/components/ui/select'
 import AppLayout from '@/layouts/app-layout'
 import { BreadcrumbItem } from '@/types'
-import { Head } from '@inertiajs/react'
-import React from 'react'
+import { Head, useForm } from '@inertiajs/react'
+import React, { useEffect, useState } from 'react'
 import { MdContactPhone } from 'react-icons/md'
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -12,31 +16,33 @@ const breadcrumbs: BreadcrumbItem[] = [
 ]
 
 type Contact = {
-    contactID: number;
-    contactName: string;
-    contactDescription: string;
-    contactPhone: string;
-    contactLevel: string;
+    id: number;
+    name: string,
+    phone: string,
+    priority_level: string,
+}
+type contactForm = {
+    name: string,
+    phone: string,
+    priority_level: string,
 }
 
-const sampleContacts: Contact[] = [
-    {
-        contactID: 1,
-        contactName: 'John Doe',
-        contactDescription: 'John Doe Description',
-        contactPhone: '1234567890',
-        contactLevel: 'Level 1'
-    },
-    {
-        contactID: 2,
-        contactName: 'Jane Doe',
-        contactDescription: 'Jane Doe Description',
-        contactPhone: '1234567890',
-        contactLevel: 'Level 2'
-    }
-]
-
 const contacts = () => {
+    const [addContact, setAddContact] = useState(false);
+    const [contact, setContact] = useState<Contact[]>([]);
+    const { data, setData, post, processing, errors, reset } = useForm<Required<contactForm>>({
+        name: '',
+        phone: '',
+        priority_level: ''
+    });
+
+    useEffect(() => {
+        fetchContacts();
+    }, []);
+
+    const fetchContacts = async () => {
+
+    }
     return (
         <>
             <AppLayout breadcrumbs={breadcrumbs}>
@@ -48,7 +54,7 @@ const contacts = () => {
                             <p className='text-sm'>Sample Description of this page</p>
                         </div>
                         <div>
-                            <button className='py-2 px-4 cursor-pointer rounded-md bg-green-400 font-semibold flex gap-2 items-center'><MdContactPhone /> Add Contact</button>
+                            <button onClick={() => setAddContact(true)} className='py-2 px-4 cursor-pointer rounded-md bg-green-400 font-semibold flex gap-2 items-center'><MdContactPhone /> Add Contact</button>
                         </div>
                     </div>
                     <hr className='mt-2 mb-5 border-slate-600' />
@@ -58,20 +64,18 @@ const contacts = () => {
                                 <tr>
                                     <th className='px-6 py-3 text-center text-sm font-semibold text-gray-700 dark:text-gray-300 uppercase tracking-wider'>#</th>
                                     <th className='px-6 py-3 text-center text-sm font-semibold text-gray-700 dark:text-gray-300 uppercase tracking-wider'>Name</th>
-                                    <th className='px-6 py-3 text-center text-sm font-semibold text-gray-700 dark:text-gray-300 uppercase tracking-wider'>Description</th>
                                     <th className='px-6 py-3 text-center text-sm font-semibold text-gray-700 dark:text-gray-300 uppercase tracking-wider'>Phone</th>
                                     <th className='px-6 py-3 text-center text-sm font-semibold text-gray-700 dark:text-gray-300 uppercase tracking-wider'>Contact Level</th>
                                     <th className='px-6 py-3 text-center text-sm font-semibold text-gray-700 dark:text-gray-300 uppercase tracking-wider'>Action</th>
                                 </tr>
                             </thead>
                             <tbody className='divide-y divide-gray-100 dark:divide-gray-700'>
-                                {sampleContacts.map((contact) => (
-                                    <tr key={contact.contactID} className='hover:bg-gray-100 dark:hover:bg-gray-800 transition'>
-                                        <td className='px-6 py-4 text-center whitespace-nowrap text-sm text-gray-900 dark:text-gray-100'>{contact.contactID}</td>
-                                        <td className='px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white'>{contact.contactName}</td>
-                                        <td className='px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500 dark:text-gray-300'>{contact.contactDescription}</td>
-                                        <td className='px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500 dark:text-gray-300'>{contact.contactPhone}</td>
-                                        <td className='px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500 dark:text-gray-300'>{contact.contactLevel}</td>
+                                {contact.map((contacts) => (
+                                    <tr key={contacts.id} className='hover:bg-gray-100 dark:hover:bg-gray-800 transition'>
+                                        <td className='px-6 py-4 text-center whitespace-nowrap text-sm text-gray-900 dark:text-gray-100'>{contacts.id}</td>
+                                        <td className='px-6 py-4 text-center whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white'>{contacts.name}</td>
+                                        <td className='px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500 dark:text-gray-300'>{contacts.phone}</td>
+                                        <td className='px-6 py-4 text-center whitespace-nowrap text-sm text-gray-500 dark:text-gray-300'>{contacts.priority_level}</td>
                                         <td className='px-6 py-4  whitespace-nowrap text-sm font-medium flex gap-2 justify-center'>
                                             <button className='px-2 py-1 rounded text-white bg-green-400 hover:bg-green-900 dark:bg-green-400'>View</button>
                                             <button className='px-2 py-1 rounded text-white bg-indigo-400 hover:bg-indigo-900 dark:bg-indigo-400'>Edit</button>
@@ -84,6 +88,67 @@ const contacts = () => {
                     </div>
                 </div>
             </AppLayout>
+            <Modal header='Add Contact' subtitle='sample subtitle' isVisible={addContact} onClose={() => setAddContact(false)} children={
+                <>
+                    <form>
+                        <div className='overflow-x-auto p-4'>
+                            <div className="flex flex-col gap-4 w-200">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="contact_name" className='text-black'>Contact Name</Label>
+                                    <Input
+                                        className='text-black'
+                                        id="contact_name"
+                                        type="text"
+                                        required
+                                        autoFocus
+                                        tabIndex={1}
+                                        value={data.name}
+                                        onChange={(e) => setData('name', e.target.value)}
+                                        placeholder="Contact Name"
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <Label htmlFor="phone" className='text-black'>Phone Number</Label>
+                                    <Input
+                                        className='text-black'
+                                        id="phone"
+                                        type="text"
+                                        required
+                                        autoFocus
+                                        tabIndex={1}
+                                        value={data.phone}
+                                        onChange={(e) => setData('phone', e.target.value)}
+                                        placeholder="+63 XXX XXXX XXX"
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <Label htmlFor="phone" className='text-black'>Priority Level</Label>
+                                    <p className='text-xs px-2'>Note: The Priority Level indicates how important a contact is during emergencies—those marked as High will be contacted first in urgent situations, Medium for moderate importance, and Low if others are unavailable or for general updates. Please choose the level that best reflects the contact’s urgency in emergency scenarios.</p>
+                                    <Select
+                                        value={data.priority_level}
+                                        onValueChange={(value) => setData('priority_level', value)}
+                                    >
+                                        <SelectTrigger id="priority" className="text-black">
+                                            <SelectValue placeholder="Select Priority Level" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="1">High</SelectItem>
+                                            <SelectItem value="2">Medium</SelectItem>
+                                            <SelectItem value="3">Low</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                            </div>
+                            <div className="mt-10 w-full bottom-10 right-10 flex flex-col sm:flex-row gap-2 sm:gap-4 justify-end">
+                                <button className="cursor-pointer bg-gray-500 text-white py-2 px-4 rounded-md" onClick={() => { setAddContact(false); reset(); }}>Cancel</button>
+                                <button type='submit' className="cursor-pointer bg-blue-500 text-white py-2 px-4 rounded-md">Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </>
+            }>
+
+            </Modal>
         </>
     )
 }
